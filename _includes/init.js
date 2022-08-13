94e00f3ba80bc7276096c87eec69987cd2ee2e6d
Liz Daly <liz7daly@gmail.com>
2022-08-13 17:57:28 +0100

    Add mobile menu and update nav styles
    - Update styles to be mobile-first
    - Add focus trap function fo mobile menu
    - Update nav and link styles

---
+++ _includes/init.js
@@ -0,0 +1,55 @@
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+const buttonText = document.getElementById("button-text");
+
+menuButton.addEventListener("click", () => {
+  let focusableEls;
+  let firstFocusableEl;
+  let lastFocusableEl;
+  const isMenuOpen = !menu.classList.contains("expanded");
+  toggleMenu();
+
+  if (isMenuOpen) {
+    focusableEls = Array.from(menu.querySelectorAll(".nav__link"));
+    firstFocusableEl = focusableEls[0];
+    lastFocusableEl = focusableEls[focusableEls.length - 1];
+    firstFocusableEl.focus();
+    menu.addEventListener("keydown", setFocusTrap);
+  } else {
+    menu.removeEventListener("keydown", setFocusTrap);
+  }
+
+  function setFocusTrap(e) {
+    if (e.key === "Tab") {
+      if (e.shiftKey) {
+        /* shift + tab */ if (document.activeElement === firstFocusableEl) {
+          lastFocusableEl.focus();
+          e.preventDefault();
+        }
+      } /* tab */ else {
+        if (document.activeElement === lastFocusableEl) {
+          console.log("on last element");
+          if (menuButton) {
+            menuButton.focus();
+          } else {
+            firstFocusableEl.focus();
+          }
+          e.preventDefault();
+        }
+      }
+    }
+
+    if (e.key === "Escape") {
+      toggleMenu();
+      menuButton.focus();
+      menu.removeEventListener('keydown',setFocusTrap);
+    }
+  }
+
+  function toggleMenu() {
+    menuButton.setAttribute("aria-expanded", isMenuOpen);
+    menu.classList.toggle("expanded");
+    buttonText.innerText = isMenuOpen ? "close" : "menu";
+  }
+});
+
