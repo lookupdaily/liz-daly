e5e388168cde5ab17ceca7a5e26e4e5a6ef3eec8
Liz Daly <liz7daly@gmail.com>
2025-02-09 21:56:08 +0000

    Add markdown definition list plugin
    This enables definition list mark up in 11ty flavoured markdown (markdown-it).

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const markdownItDefList = require("markdown-it-deflist");
+
 module.exports = (eleventyConfig) => {
 	eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItDefList));
 
 	return {
 		passthroughFileCopy: true,
